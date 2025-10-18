ad23e59a47eea6226a5008925ab35f1964be74e6
Nobodip Debnath <nobodipdebnath45@.com>
2025-10-18 20:05:29 +0600

    log in for add all page in mobile device

--- script/login.js
+++ script/login.js
@@ -2,7 +2,7 @@ document.getElementById('login').addEventListener('click', function(event){
     event.preventDefault();
     const userEmail = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
-    if(userEmail === 'nobodipdebnath45@gmail.com' && password ==='nobodip667733'){
+    if(userEmail === 'nobodipdebnath45@gmail.com' && password ==='1234'){
         alert('You are log in')
         window.location.href = '../page/dashboard.html'
     }
