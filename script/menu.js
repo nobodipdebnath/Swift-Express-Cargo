b9bf8386e327e0d907649276d178c9ee1dccd6d0
Nobodip Debnath <nobodipdebnath45@.com>
2025-10-19 00:11:48 +0600

    dashboard mobile menu compleat

--- script/menu.js
+++ script/menu.js
@@ -1,8 +1,44 @@
- var navLinks = document.getElementById("navLinks");
 
-    function ShowMenu(){
-        navLinks.style.right = "0";
+const menuIcon = document.getElementById("show-menu");
+const closeIcon = document.getElementById("hide-menu");
+const mobileMenu = document.getElementById("all-links");
+
+if (menuIcon && mobileMenu && closeIcon) {
+  const openMenu = () => {
+    mobileMenu.classList.remove("-left-[100%]");
+    mobileMenu.classList.add("left-0");
+  };
+
+  const closeMenu = () => {
+    mobileMenu.classList.add("-left-[100%]");
+    mobileMenu.classList.remove("left-0");
+  };
+
+
+  menuIcon.addEventListener("click", (e) => {
+    e.stopPropagation();
+    openMenu();
+  });
+
+  // close menu
+  closeIcon.addEventListener("click", (e) => {
+    e.stopPropagation();
+    closeMenu();
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      !mobileMenu.contains(event.target) &&
+      !menuIcon.contains(event.target)
+    ) {
+      closeMenu();
     }
-    function hideMenu(){
-        navLinks.style.right = "-200px";
-    }
+  });
+
+  const menuItems = mobileMenu.querySelectorAll("a, li, button");
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      closeMenu();
+    });
+  });
+}
